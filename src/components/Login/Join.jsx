3d7dc10aced9d07985e3b252839e43acced57ec5
SJLEE <akacast01@gmail.com>
2023-06-26 21:13:21 +0900

    Join수정

--- src/components/Login/Join.jsx
+++ src/components/Login/Join.jsx
@@ -1,6 +1,64 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import oc from 'open-color';
+
+const Text = styled.div`
+    font-size: 30px;
+    color: transparent;
+    letter-spacing: 1px;
+    font-family: 'NanumGothic', sans-serif;
+    width: 80%;
+    margin: 0 auto;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    font-weight: bold;
+    text-align: center;
+    background: linear-gradient(to right, ${oc.teal[6]}, ${oc.cyan[5]});
+    -webkit-background-clip: text; /* Safari */
+    -webkit-text-fill-color: transparent; /* Safari */
+`;
+
+const Wrapper = styled.div`
+    font-size: 20px;
+    font-family: 'NanumGothic', sans-serif;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 30%;
+    margin: auto;
+    background-color: rgba(255, 255, 255, 0.8);
+    padding: 0px 24px;
+    padding-bottom: 24px;
+    border-radius: 16px;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.25);
+    margin-bottom: 24px;
+
+    input[type="text"],
+    input[type="number"],
+    select {
+        width: 100%;
+        font-size: inherit;
+    }
+`;
+
+const Button = styled.button`
+    padding: 8px 30px;
+    color: #FFFFFF;
+    border: none;
+    border-radius: 10px;
+    font-size: 20px;
+    cursor: pointer;
+    margin-top: 10px;
+    background: linear-gradient(to right, ${oc.teal[6]}, ${oc.cyan[5]});
+    align: center;
+
+    &:hover {
+    /* 클릭시 아래로 미세하게 움직임 */
+    transform: translateY(3px);
+    }
+`;
 
 
 const Join = ({ isLoggedIn, setIsLoggedIn }) => {
@@ -41,22 +99,44 @@ const Join = ({ isLoggedIn, setIsLoggedIn }) => {
     return (
         <div>
             <br></br><br></br><br></br>
-            <h1>가입을 축하합니다!</h1>
-            <div>
-                <input value={nameInputText} onChange={(event) => setNameInputText(event.target.value)} placeholder="닉네임을 입력해주세요"/>
-            </div>
-            <div>
-                <select value={sex} onChange={(event) => setSex(event.target.value)}>
-                    <option value="">성별을 선택해주세요</option>
-                    <option value="male">남자</option>
-                    <option value="female">여자</option>
-                </select>
-            </div>
-            <div>
-                <input type="number" value={ageInputText} onChange={(event) => setAgeInputText(event.target.value)} placeholder="나이를 입력해주세요"/>
-            </div>
-            <button onClick={handleSave}>확인</button>
+            <Text>가입을 축하합니다!</Text>
+            <Wrapper>
+                <h2>정보입력</h2>
+                <form>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td style={{ paddingRight: '10px' }}>닉네임</td>
+                                <td style={{ paddingLeft: '10px' }}>
+                                    <input type="text" value={nameInputText} onChange={(event) => setNameInputText(event.target.value)} placeholder="닉네임을 입력해주세요"/>
+                                </td>
+                            </tr>
+                            <tr><br></br></tr>
+                            <tr>
+                            <td style={{ paddingRight: '10px' }}>성별</td>
+                            <td style={{ paddingLeft: '10px' }}>
+                                <select value={sex} onChange={(event) => setSex(event.target.value)}>
+                                    <option value="">성별을 선택해주세요</option>
+                                    <option value="male">남자</option>
+                                    <option value="female">여자</option>
+                                </select>
+                            </td>
+                            </tr>
+                            <tr><br></br></tr>
+                            <tr>
+                                <td style={{ paddingRight: '10px' }}>나이</td>
+                                <td style={{ paddingLeft: '10px' }}>
+                                    <input type="number" value={ageInputText} onChange={(event) => setAgeInputText(event.target.value)} placeholder="나이를 입력해주세요"/>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <br />
+                </form>
+                <Button onClick={handleSave}>확인</Button>
+            </Wrapper>
         </div>
+        
         
     );
 }
